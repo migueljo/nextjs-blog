ad9a669e276acde15452283dba13c0642dd4e435
Miguel Martínez <mmartinez@livevox.com>
2021-03-07 18:11:59 -0500

    Finishing blog

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
+import Link from 'next/link'
 
+import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout'
 import { getSortedPostsData } from '../lib/posts'
 import utilStyles from '../styles/utils.module.css'
 
 export default function Home({ allPostsData }) {
-  console.log({ allPostsData })
   return (
     <Layout home>
       <Head>
@@ -23,11 +24,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
